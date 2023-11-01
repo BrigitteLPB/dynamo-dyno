05b0299d374798e49c78def9d92cbbf234170a16
BrigitteLPB <lpb.brigitte@gmail.com>
2023-11-01 17:50:34 +0100

    remove the setting page

--- ui/modules/apps/DynamicDyno/app.js
+++ ui/modules/apps/DynamicDyno/app.js
@@ -68,7 +68,7 @@ class VehicleCurves {
      * return a js object
      * @returns
      */
-    getObject(){
+    getObject() {
         return {
             current: this.current,
             max: this.max,
@@ -79,15 +79,15 @@ class VehicleCurves {
      * Add a new engine to the vehicle
      * @param {*} engine
      */
-    addEngine(engine){
+    addEngine(engine) {
         console.debug(JSON.stringify(engine));
         console.debug(engine.priority);
 
-        if(engine.priority < this.enginePriority){
+        if (engine.priority < this.enginePriority) {
             return;
         }
 
-        if(engine.priority > this.enginePriority){
+        if (engine.priority > this.enginePriority) {
             this.enginePriority = engine.priority;
 
             // torque
@@ -99,13 +99,13 @@ class VehicleCurves {
             this.max.power.curve = [];
         }
 
-        for (let e of this.engines){
+        for (let e of this.engines) {
             let isEqual = false;
             e.torque.forEach((t, i) => {
                 isEqual = t == engine.torque[i];
             });
 
-            if(isEqual){
+            if (isEqual) {
                 console.debug('already have this engine');
                 return;
             }
@@ -150,7 +150,7 @@ class VehicleCurves {
      * @param {double} rpm
      * @param {double} torque
      */
-    updateCurrentValues(rpm, torque){
+    updateCurrentValues(rpm, torque) {
         const rpmInd = Math.min(Math.floor(rpm), this.maxRPM - 1);
         const power = ((2 * Math.PI * rpm * torque) / 60) / 736;
 
@@ -175,7 +175,7 @@ class VehicleCurves {
         }
     }
 
-    resetCurrent(){
+    resetCurrent() {
         // torque
         this.current.torque.max = 0;
         this.current.torque.curve = new Array(Math.floor(this.maxRPM / (100 - VehicleCurves.PRECISION_FACTOR))).fill(0);
@@ -190,62 +190,45 @@ angular.module('beamng.apps')
         function ($log, CanvasShortcuts) {
             return {
                 template: `
-                    <div
-                        style="position: relative; display: flex; flex-direction: column; width: 100%; height: 100%; margin: 0; padding: 0; background-color: whitesmoke;">
-                        <div style="position: relative; flex: 1 0 auto;">
-                            <canvas style="position: absolute; top: 0; left: 0; margin: 0; padding: 0;"></canvas>
-                            <canvas style="position: absolute; top: 0; left: 0; margin: 0; padding: 0;"></canvas>
-                            <canvas style="position: absolute; top: 0; left: 0; margin: 0; padding: 0;"></canvas>
-                        </div>
-                        <div style="display: flex; flex-direction: row; justify-content: space-around; width: 100%; padding: 6px 0;">
-                            <div style="display: flex; flex-direction: column; width: 40%; height: inherit;"
-                                ng-repeat="(key, obj) in currentData">
-                                <h3 style="text-align: center; width: 100%; font-family: monospace; padding: 0; margin: 0;">{{ obj.name }}
-                                </h3>
-                                <div style="display: flex; flex-direction: row; justify-content: space-between;">
-                                    <graph-legend-tip type="line" color="black" dash-array="{{ obj.dashArray }}"></graph-legend-tip>
-                                    <span style="font-family: monospace;">{{::"ui.apps.torquecurve.Torque" | translate}}</span>
-                                    <span style="font-family: monospace; width: 100%; text-align: center;">{{ obj.torque.val.toFixed(2)
-                                        }}/{{ obj.torque.max.toFixed(0)
-                                        }}</span>
-                                    <span style="font-family: monospace;">[{{ obj.power.units }}]</span>
-                                </div>
-                                <div style="display: flex; flex-direction: row; justify-content: space-between;">
-                                    <graph-legend-tip type="line" color="red" dash-array="{{ obj.dashArray }}"></graph-legend-tip>
-                                    <span style="font-family: monospace;">{{::"ui.apps.torquecurve.Power" | translate}}</span>
-                                    <span style="font-family: monospace; width: 100%; text-align: center;">{{ obj.power.val.toFixed(2) }}/{{
-                                        obj.power.max.toFixed(0)
-                                        }}</span>
-                                    <span style="font-family: monospace;">[{{ obj.power.units }}]</span>
-                                </div>
+                <div
+                    style="position: relative; display: flex; flex-direction: column; width: 100%; height: 100%; margin: 0; padding: 0; background-color: whitesmoke;">
+                    <div style="position: relative; flex: 1 0 auto;">
+                        <canvas style="position: absolute; top: 0; left: 0; margin: 0; padding: 0;"></canvas>
+                        <canvas style="position: absolute; top: 0; left: 0; margin: 0; padding: 0;"></canvas>
+                        <canvas style="position: absolute; top: 0; left: 0; margin: 0; padding: 0;"></canvas>
+                    </div>
+                    <div style="display: flex; flex-direction: row; justify-content: space-around; width: 100%; padding: 6px 0;">
+                        <div style="display: flex; flex-direction: column; width: 40%; height: inherit;"
+                            ng-repeat="(key, obj) in currentData">
+                            <h3 style="text-align: center; width: 100%; font-family: monospace; padding: 0; margin: 0;">{{ obj.name }}
+                            </h3>
+                            <div style="display: flex; flex-direction: row; justify-content: space-between;">
+                                <graph-legend-tip type="line" color="black" dash-array="{{ obj.dashArray }}"></graph-legend-tip>
+                                <span style="font-family: monospace;">{{::"ui.apps.torquecurve.Torque" | translate}}</span>
+                                <span style="font-family: monospace; width: 100%; text-align: center;">{{ obj.torque.val.toFixed(2)
+                                    }}/{{ obj.torque.max.toFixed(0)
+                                    }}</span>
+                                <span style="font-family: monospace;">[{{ obj.torque.units }}]</span>
                             </div>
-                            <div style="display: flex; flex-direction: column; justify-content: space-evenly;">
-                                <button style="border: none; background: transparent;" ng-click="/*displaySettings(true);*/">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-                                        <path
-                                            d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" />
-                                    </svg>
-                                </button>
-                                <button style="border: none; background: transparent;" ng-click="resetCurrentData()">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                                        <path
-                                            d="M566.6 9.4c12.5 12.5 12.5 32.8 0 45.3l-192 192 34.7 34.7c4.2 4.2 6.6 10 6.6 16c0 12.5-10.1 22.6-22.6 22.6H364.3L256 211.7V182.6c0-12.5 10.1-22.6 22.6-22.6c6 0 11.8 2.4 16 6.6l34.7 34.7 192-192c12.5-12.5 32.8-12.5 45.3 0zm-344 225.5L341.1 353.4c3.7 42.7-11.7 85.2-42.3 115.8C271.4 496.6 234.2 512 195.5 512L22.1 512C9.9 512 0 502.1 0 489.9c0-6.3 2.7-12.3 7.3-16.5L133.7 359.7c4.2-3.7-.4-10.4-5.4-7.9L77.2 377.4c-6.1 3-13.2-1.4-13.2-8.2c0-31.5 12.5-61.7 34.8-84l8-8c30.6-30.6 73.1-45.9 115.8-42.3zM464 352a80 80 0 1 1 0 160 80 80 0 1 1 0-160z" />
-                                    </svg>
-                                </button>
+                            <div style="display: flex; flex-direction: row; justify-content: space-between;">
+                                <graph-legend-tip type="line" color="red" dash-array="{{ obj.dashArray }}"></graph-legend-tip>
+                                <span style="font-family: monospace;">{{::"ui.apps.torquecurve.Power" | translate}}</span>
+                                <span style="font-family: monospace; width: 100%; text-align: center;">{{ obj.power.val.toFixed(2) }}/{{
+                                    obj.power.max.toFixed(0)
+                                    }}</span>
+                                <span style="font-family: monospace;">[{{ obj.power.units }}]</span>
                             </div>
                         </div>
-                        <div
-                            style="position: absolute; top: 0; width: 0; width: 100%; height: 100%; z-index: 1; background-color: whitesmoke; display: {{show_settings ? 'visible' : 'none'}};">
-                            <button style="border: none; background: transparent;" ng-click="displaySettings(false);">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                        <div style="display: flex; flex-direction: column; justify-content: space-evenly;">
+                            <button style="border: none; background: transparent;" ng-click="resetCurrentData()">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
                                     <path
-                                        d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
+                                        d="M566.6 9.4c12.5 12.5 12.5 32.8 0 45.3l-192 192 34.7 34.7c4.2 4.2 6.6 10 6.6 16c0 12.5-10.1 22.6-22.6 22.6H364.3L256 211.7V182.6c0-12.5 10.1-22.6 22.6-22.6c6 0 11.8 2.4 16 6.6l34.7 34.7 192-192c12.5-12.5 32.8-12.5 45.3 0zm-344 225.5L341.1 353.4c3.7 42.7-11.7 85.2-42.3 115.8C271.4 496.6 234.2 512 195.5 512L22.1 512C9.9 512 0 502.1 0 489.9c0-6.3 2.7-12.3 7.3-16.5L133.7 359.7c4.2-3.7-.4-10.4-5.4-7.9L77.2 377.4c-6.1 3-13.2-1.4-13.2-8.2c0-31.5 12.5-61.7 34.8-84l8-8c30.6-30.6 73.1-45.9 115.8-42.3zM464 352a80 80 0 1 1 0 160 80 80 0 1 1 0-160z" />
                                 </svg>
                             </button>
-                            <md-slider min=0 max=100 ng-model="precisionFactor" step="1"></md-slider>
-                            <md-slider min=0 max=100 ng-model="minTorque" step="0.5"></md-slider>
                         </div>
                     </div>
+                </div>
                 `,
                 replace: true,
                 restrict: 'EA',
@@ -259,10 +242,6 @@ angular.module('beamng.apps')
                     $scope.minTorque = 1;
 
                     $scope.show_settings = false;
-
-                    $scope.displaySettings = function (state) {
-                        $scope.show_settings = state;
-                    }
 
                     $scope.resetCurrentData = function () {
                         $scope.vehicles[$scope.vehicleID].resetCurrent();
@@ -390,14 +369,14 @@ angular.module('beamng.apps')
                             console.log(`got a new vehicle, id : ${data.vehicleID}`);
                             scope.vehicleID = data.vehicleID;
 
-                            if(!(data.vehicleID in scope.vehicles)){
+                            if (!(data.vehicleID in scope.vehicles)) {
                                 scope.vehicles[data.vehicleID] = VehicleCurves.makeDefault(data.vehicleID, data.maxRPM)
                             }
                         }
 
-                        if(scope.vehicleID in scope.vehicles){
+                        if (scope.vehicleID in scope.vehicles) {
                             console.debug(`add a new engine for vehicle ${scope.vehicleID}`)
-                            for(let engine of data.curves){
+                            for (let engine of data.curves) {
                                 scope.vehicles[data.vehicleID].addEngine(engine);
                             }
 
@@ -422,7 +401,7 @@ angular.module('beamng.apps')
 
                             scope.vehicles[scope.vehicleID].updateCurrentValues(_rpm, torque);
                             scope.currentData = scope.vehicles[scope.vehicleID].getObject();
-                            
+
                             plotDynamicGraphs(scope.vehicles[scope.vehicleID]);
                             drawCursor(_rpm, scope.vehicles[scope.vehicleID]);
                         }
@@ -453,4 +432,6 @@ angular.module('beamng.apps')
                     })
                 }
             }
-        }])
+        }
+    ]
+    )
