4d575adc7ec613873b5ecfcbedb9abbff18641f8
BrigitteLPB <lpb.brigitte@gmail.com>
2023-11-16 21:18:10 +0100

    add black version

--- ui/modules/apps/DynamicDynoBlack/app.js
+++ ui/modules/apps/DynamicDynoBlack/app.js
@@ -1,6 +1,6 @@
 
 angular.module('beamng.apps')
-    .directive('dynamicDyno', ['$log', 'CanvasShortcuts',
+    .directive('dynamicDynoBlack', ['$log', 'CanvasShortcuts',
         function ($log, CanvasShortcuts) {
             class DrawBoard {
                 static PLOT_MARGINS = { top: 15, bottom: 25, left: 25, right: 26 };
@@ -41,10 +41,10 @@ angular.module('beamng.apps')
                     var powerTicks = Array(6).fill().map((x, i, a) => i * maxPower / (a.length - 1));
                     var rpmTicks = Array(Math.floor(vehicle.maxRPM / 1000) + 1).fill().map((x, i) => i * 1000);
 
-                    this.CanvasShortcuts.plotAxis(this.ui_ctx, 'left', [0, maxTorque], torqueTicks, DrawBoard.PLOT_MARGINS, { numLines: torqueTicks.length, color: 'var(--dark-neutral-grey)', dashArray: [2, 3] }, 'black');
+                    this.CanvasShortcuts.plotAxis(this.ui_ctx, 'left', [0, maxTorque], torqueTicks, DrawBoard.PLOT_MARGINS, { numLines: torqueTicks.length, color: 'lightgrey', dashArray: [2, 3] }, 'white');
                     this.CanvasShortcuts.plotAxis(this.ui_ctx, 'right', [0, maxPower], powerTicks, DrawBoard.PLOT_MARGINS, null, 'red');
                     this.CanvasShortcuts.plotAxis(this.ui_ctx, 'top', [0, vehicle.maxRPM], [], DrawBoard.PLOT_MARGINS, null);
-                    this.CanvasShortcuts.plotAxis(this.ui_ctx, 'bottom', [0, vehicle.maxRPM], rpmTicks, DrawBoard.PLOT_MARGINS, { values: rpmTicks, color: 'var(--dark-neutral-grey)', dashArray: [2, 3] }, 'black');
+                    this.CanvasShortcuts.plotAxis(this.ui_ctx, 'bottom', [0, vehicle.maxRPM], rpmTicks, DrawBoard.PLOT_MARGINS, { values: rpmTicks, color: 'lightgrey', dashArray: [2, 3] }, 'white');
 
                     this.CanvasShortcuts.plotData(this.ui_ctx, new Array(vehicle.max.torque.curve.length).fill(0), 0, maxTorque, { margin: DrawBoard.PLOT_MARGINS, lineWidth: 0 });
                     this.CanvasShortcuts.plotData(this.ui_ctx, new Array(vehicle.max.power.curve.length).fill(0), 0, maxPower, { margin: DrawBoard.PLOT_MARGINS, lineWidth: 0 });
@@ -66,7 +66,7 @@ angular.module('beamng.apps')
                     const maxPower = Math.ceil(vehicle.max.power.max / DrawBoard.VALUE_INTERVAL) * DrawBoard.VALUE_INTERVAL;
 
                     // draw static curve
-                    this.CanvasShortcuts.plotData(this.max_ctx, vehicle.max.torque.curve.map(key => UiUnits.torque(key).val), 0, maxTorque, { margin: DrawBoard.PLOT_MARGINS, lineWidth: 2, lineColor: 'black', dashArray: vehicle.max.dashArray });
+                    this.CanvasShortcuts.plotData(this.max_ctx, vehicle.max.torque.curve.map(key => UiUnits.torque(key).val), 0, maxTorque, { margin: DrawBoard.PLOT_MARGINS, lineWidth: 2, lineColor: 'white', dashArray: vehicle.max.dashArray });
                     this.CanvasShortcuts.plotData(this.max_ctx, vehicle.max.power.curve.map(key => UiUnits.power(key).val), 0, maxPower, { margin: DrawBoard.PLOT_MARGINS, lineWidth: 2, lineColor: 'red', dashArray: vehicle.max.dashArray });
                 }
 
@@ -82,7 +82,7 @@ angular.module('beamng.apps')
                     const maxPower = Math.ceil(vehicle.max.power.max / DrawBoard.VALUE_INTERVAL) * DrawBoard.VALUE_INTERVAL;
 
                     // draw dynamic curve
-                    this.CanvasShortcuts.plotData(this.dyno_ctx, vehicle.current.torque.curve.map(key => UiUnits.torque(key).val), 0, maxTorque, { margin: DrawBoard.PLOT_MARGINS, lineWidth: 2, lineColor: 'black', dashArray: vehicle.current.dashArray });
+                    this.CanvasShortcuts.plotData(this.dyno_ctx, vehicle.current.torque.curve.map(key => UiUnits.torque(key).val), 0, maxTorque, { margin: DrawBoard.PLOT_MARGINS, lineWidth: 2, lineColor: 'white', dashArray: vehicle.current.dashArray });
                     this.CanvasShortcuts.plotData(this.dyno_ctx, vehicle.current.power.curve.map(key => UiUnits.power(key).val), 0, maxPower, { margin: DrawBoard.PLOT_MARGINS, lineWidth: 2, lineColor: 'red', dashArray: vehicle.current.dashArray });
                 }
 
@@ -315,7 +315,7 @@ angular.module('beamng.apps')
             return {
                 template: `
                 <div
-                    style="position: relative; display: flex; flex-direction: column; width: 100%; height: 100%; margin: 0; padding: 0; background-color: var(--white-1); color: var(--bng-black-8);">
+                    style="position: relative; display: flex; flex-direction: column; width: 100%; height: 100%; margin: 0; padding: 0; background-color: var(--bng-black-8); color: var(--white-1);">
                     <div style="position: relative; flex: 1 0 auto;">
                         <canvas style="position: absolute; top: 0; left: 0; margin: 0; padding: 0;"></canvas>
                         <canvas style="position: absolute; top: 0; left: 0; margin: 0; padding: 0;"></canvas>
@@ -328,7 +328,7 @@ angular.module('beamng.apps')
                             <h3 style="text-align: center; width: 100%; font-family: monospace; padding: 0; margin: 0;">{{ obj.name }}
                             </h3>
                             <div style="display: flex; flex-direction: row; justify-content: space-between;">
-                                <graph-legend-tip type="line" color="black" dash-array="{{ obj.dashArray }}"></graph-legend-tip>
+                                <graph-legend-tip type="line" color="white" dash-array="{{ obj.dashArray }}"></graph-legend-tip>
                                 <span style="font-family: monospace;">{{::"ui.apps.torquecurve.Torque" | translate}}</span>
                                 <span style="font-family: monospace; width: 100%; text-align: center;">{{ obj.torque.val.toFixed(2)
                                     }}/{{ obj.torque.max.toFixed(0)
@@ -346,19 +346,19 @@ angular.module('beamng.apps')
                         </div>
                         <div style="display: flex; flex-direction: column; justify-content: space-evenly;">
                             <button style="border: thin solid darkgrey; border-radius: 3px; background: transparent;" ng-click="resetCurrentData()">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" style="fill: white">
                                     <path
                                         d="M566.6 9.4c12.5 12.5 12.5 32.8 0 45.3l-192 192 34.7 34.7c4.2 4.2 6.6 10 6.6 16c0 12.5-10.1 22.6-22.6 22.6H364.3L256 211.7V182.6c0-12.5 10.1-22.6 22.6-22.6c6 0 11.8 2.4 16 6.6l34.7 34.7 192-192c12.5-12.5 32.8-12.5 45.3 0zm-344 225.5L341.1 353.4c3.7 42.7-11.7 85.2-42.3 115.8C271.4 496.6 234.2 512 195.5 512L22.1 512C9.9 512 0 502.1 0 489.9c0-6.3 2.7-12.3 7.3-16.5L133.7 359.7c4.2-3.7-.4-10.4-5.4-7.9L77.2 377.4c-6.1 3-13.2-1.4-13.2-8.2c0-31.5 12.5-61.7 34.8-84l8-8c30.6-30.6 73.1-45.9 115.8-42.3zM464 352a80 80 0 1 1 0 160 80 80 0 1 1 0-160z" />
                                 </svg>
                             </button>
                             <button style="border: thin solid darkgrey; border-radius: 3px; background: transparent;" ng-click="hideStaticCurve()">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"
-                                    style="display: {{drawBoard.isStaticCurveHide ? 'none' : 'block'}};">
+                                    style="display: {{drawBoard.isStaticCurveHide ? 'none' : 'block'}}; fill: white">
                                     <path
                                         d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z" />
                                 </svg>
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"
-                                    style="display: {{drawBoard.isStaticCurveHide ? 'block' : 'none'}};">
+                                    style="display: {{drawBoard.isStaticCurveHide ? 'block' : 'none'}}; fill: white">
                                     <path
                                         d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
                                 </svg>
